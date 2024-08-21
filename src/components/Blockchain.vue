5bd8f848152441a9be89744f1aa59b0f30ed40e9
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-08-21 15:56:06 +0200

    api blocchi

--- src/components/Blockchain.vue
+++ src/components/Blockchain.vue
@@ -1,15 +1,32 @@
-<script >
+<script>
+import axios from "axios";
 export default {
-    name:'Blockchain'
-}
+  name: "Blockchain",
+  data() {
+    return {};
+  },
+  methods: {
+    async call() {
+      try {
+        const data = await axios("https://blockstream.info/api/blocks/tip/height");  // altezza ultimo blocco
+        const data1 = await axios("https://blockstream.info/api/block-height/88"); // blocco specifico 
+        const data2 = await axios("https://blockstream.info/api/block/" + data1.data); // dati blocco
+        console.log(data2.data);
+        console.log(data1.data);
+        console.log(data);
+      } catch (error) {
+        console.log(error.data);
+      }
+    },
+  },
+  mounted() {
+    this.call();
+  },
+};
 </script>
 
 <template>
-    <div>
-        ciaoaoaoa
-    </div>
+  <div>ciaoaoaoa</div>
 </template>
 
-<style scoped lang="sass">
-
-</style>
+<style scoped lang="sass"></style>
