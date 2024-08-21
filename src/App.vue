e39c65e8832099d6260b980b5bbd096781ff0ed6
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-08-21 14:38:15 +0200

    start

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import Blockchain from './components/Blockchain.vue'
+export default {
+  components: {
+    Blockchain
+  }
+}
+</script>
+
+<template>
+  <Blockchain msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Blockchain.vue
@@ -0,0 +1,17 @@
+<script >
+
+export default {
+    name:'Blockchain'
+}
+
+</script>
+
+<template>
+    <div>
+        ciaoaoaoa
+    </div>
+</template>
+
+<style scoped>
+
+</style>
