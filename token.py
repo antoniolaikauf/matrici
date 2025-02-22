5a9a9c7fde54b0d07b514ed349ed3b1602b56e63
antoniolaikauf <antonio.laikauf00@gmail.com>
2025-02-22 18:47:37 +0100

    tokeizator

--- token.py
+++ token.py
@@ -1,7 +1,7 @@
 # type: ignore
 phrase = 'ciao, mi ciao'
 
-class tokenization():
+class Tokenizer():
     
     def __init__(self, phrase):
         self.phrase = phrase
@@ -9,16 +9,47 @@ class tokenization():
     def phraseAscii(self):
         return [ ord(c) for c in self.phrase]
     
-    def merge(self):
+    def get_stats(self, phrase):
         count = {}
-        phrase = self.phraseAscii()
         for x in range(len(phrase)):
             pair = tuple(phrase[x : x + 2]) # trasformarlo in un tuple perchè non si possono usare direttamente le liste 
             count[pair] = count.get(pair, 0) + 1
         
         return count
+    
+    def merge(self):
+        phrase = self.phraseAscii()
+        stats = self.get_stats(phrase)
+        maxPair = max(stats, key=stats.get)
+        count = 256
+        print(phrase)
+        while stats and max(stats.values()) > 1:
+            print("Merging pair:", maxPair, "with frequency:", stats[maxPair])
+            stats.pop(maxPair)
+            count += 1 
+            x = 0
+            while x < len(phrase) - 1:
+                if phrase[x] == maxPair[0] and phrase[x + 1] == maxPair[1]:
+                #  Sostituisci il primo elemento della coppia con il nuovo token...
+                    phrase[x] = count
+                    # ...e rimuovi il secondo elemento
+                    del phrase[x + 1]
+                    # Non aumentiamo x perché la lista si è accorciata
+                else:
+                    x += 1 
+            #     if phrase[x:x + 2] == list(maxPair):
+            #         phrase.pop(x + 1) 
+            #         phrase[x] = count
+            print(phrase)
+            stats = self.get_stats(phrase)
+            print(maxPair)
+            maxPair = max(stats, key=stats.get)
+            print(stats)
 
 
-token = tokenization(phrase)
+        
+
+
+token = Tokenizer(phrase)
 
 print(token.merge())
