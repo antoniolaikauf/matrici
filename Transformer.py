ae4d73caafecc00c547cd451230480ccab1fec13
antoniolaikauf <antonio.laikauf00@gmail.com>
2025-03-21 20:54:42 +0100

    Transformers vector

--- Transformer.py
+++ Transformer.py
@@ -1,9 +1,25 @@
 import numpy as np
+import math
 
-
-inputWord = "hello"
-
-tokenInput = 
-class Transformers:
+class token:
     def __init__(self):
+        self.Token = 'hello'
+        self.distribution = math.sqrt(6/256)
+
+    def tokenInput(self):
+        return [ord(char) for char in self.Token]
+    
+    def tokenInputVector(self):
+        return {tk : np.full(256, self.distribution) for tk in self.tokenInput()}
+        
+class Transformers(token):
+    def __init__(self):
+        super().__init__()
+
+    def tr(self):
+        print(self.distribution)
+        print(self.tokenInputVector())
+
+t =Transformers()
+print(t.tr())
         
