8d54de802af16005967e6708246b00c059dcb4f9
antoniolaikauf <antonio.laikauf00@gmail.com>
2025-03-23 10:49:39 +0100

    comment

--- Transformer.py
+++ Transformer.py
@@ -17,7 +17,9 @@ class Tokenizer:
 
         for idxToken in range(len(tokens) - 1):            
             pair = tuple(tokens[idxToken : idxToken + 2])
+            #if the pair doesn't increse we create a new pair 
             if (pair not in dictionary ): dictionary[pair] = 1
+            # if the pair exist we increase by 1
             else: dictionary[pair] +=1
 
         return dictionary
@@ -32,41 +34,51 @@ class Tokenizer:
         
         # cicle until you don't find any pair that appears more than two or ypu have finished the max number for merge 
         while ((max(tokensPairs.values()) > 1) and (count <= self.maxMerge)):
-            print(f"Merging pair: {maxPair} with frequency: {tokensPairs[maxPair]} count {count}")
+            print(f"Merging pair: {maxPair} with frequency: {tokensPairs[maxPair]}")
+
             for idxPair in range(len(tokens) - 1):
+                # if we have a match we change the phrase and we put in the merge 
                 if(tokens[idxPair] == maxPair[0] and tokens[idxPair + 1] == maxPair[1]):
                     self.vocab[self.maxToken] = maxPair
                     tokens[idxPair] = self.maxToken
+                    # we delete the extra token 
                     del tokens[idxPair + 1]
                     self.maxToken += 1
                     break
-            
+            # we create the new pair token with the new merge 
             tokensPairs = self.pairs(tokens)
             maxPair = max(tokensPairs, key=tokensPairs.get)
+            
             count += 1
         
         print(f"The phrase is: {tokens}")
         return tokens
 
     def decode(self, phrase, text= False):
+        # the idx we increase by 1 because if a pair es. 276 is the merge between 256 and 257 if we increase by two 
+        # we could skip the element 257 that is a merge 
+
         idx = 0
+        # check if the element exist in the dictionary
         assert self.vocab.get(phrase[idx]) != None , f'carattere non esiste nel dizionario {phrase[idx]}'
 
+        # while loop over all the element in phrase 
         while (self.vocab.get(phrase[idx]) and idx != len(phrase) - 1):
-
-
+            # take the pair in the vocab
             element = self.vocab.get(phrase[idx])
-            if len(element) == 1:
-                phrase[idx] = element[0]
-                idx += 1
-            else:
-                phrase[idx] = element[0]
+            # we change the element in phrase 
+            phrase[idx] = element[0]
+            if len(element) > 1:
+            # if the len of the pair is more than 1 we change the second element
                 phrase.insert(idx + 1, element[1])
-                idx += 2
-            
-            assert self.vocab.get(phrase[idx]) != None , f'carattere non esiste nel dizionario {phrase[idx]}'
         
-        if(text): phrase = ''.join(chr(x) for x in phrase)
+            idx += 1
+            
+            # check if the element exist in the dictionary
+            assert self.vocab.get(phrase[idx]) != None , f'carattere non esiste nel dizionario {phrase[idx]}'
+        # convert to char
+        if (text): phrase = ''.join(chr(x) for x in phrase)
+
         return phrase
     
     def tokenInputVector(self):
