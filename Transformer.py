ad00ea9f09f45836ce80956bf54fd96d2dada145
antoniolaikauf <antonio.laikauf00@gmail.com>
2025-03-23 11:49:26 +0100

    fix bug

--- Transformer.py
+++ Transformer.py
@@ -34,21 +34,33 @@ class Tokenizer:
         
         # cicle until you don't find any pair that appears more than two or ypu have finished the max number for merge 
         while ((max(tokensPairs.values()) > 1) and (count <= self.maxMerge)):
-            print(f"Merging pair: {maxPair} with frequency: {tokensPairs[maxPair]}")
 
-            for idxPair in range(len(tokens) - 1):
-                # if we have a match we change the phrase and we put in the merge 
-                if(tokens[idxPair] == maxPair[0] and tokens[idxPair + 1] == maxPair[1]):
-                    self.vocab[self.maxToken] = maxPair
-                    tokens[idxPair] = self.maxToken
-                    # we delete the extra token 
-                    del tokens[idxPair + 1]
-                    self.maxToken += 1
-                    break
+            idxPair = 0
+            while idxPair < len(tokens) - 1:
+                if tokens[idxPair] == maxPair[0] and tokens[idxPair + 1] == maxPair[1]:
+                    # sostituzione della prima coppia dalla frase 
+                    tokenToUse = None
+                    for token, pair in self.vocab.items():
+                        if pair == maxPair:
+                            tokenToUse = token  # Uso il token esistente
+                            break
+                    
+                    if tokenToUse == None:
+                        tokenToUse = self.maxToken
+                        self.vocab[self.maxToken] = maxPair
+                        self.maxToken += 1 
+
+                    tokens[idxPair] = tokenToUse
+                    # rimozione della seconda coppia dalla frase
+                    del tokens[idxPair + 1] 
+
+                else:
+                    idxPair += 1 
+                    
             # we create the new pair token with the new merge 
             tokensPairs = self.pairs(tokens)
             maxPair = max(tokensPairs, key=tokensPairs.get)
-            
+
             count += 1
         
         print(f"The phrase is: {tokens}")
@@ -57,13 +69,14 @@ class Tokenizer:
     def decode(self, phrase, text= False):
         # the idx we increase by 1 because if a pair es. 276 is the merge between 256 and 257 if we increase by two 
         # we could skip the element 257 that is a merge 
-
         idx = 0
+        print(self.vocab)
         # check if the element exist in the dictionary
         assert self.vocab.get(phrase[idx]) != None , f'carattere non esiste nel dizionario {phrase[idx]}'
 
         # while loop over all the element in phrase 
         while (self.vocab.get(phrase[idx]) and idx != len(phrase) - 1):
+            print(phrase)
             # take the pair in the vocab
             element = self.vocab.get(phrase[idx])
             # we change the element in phrase 
@@ -83,7 +96,12 @@ class Tokenizer:
     
     def tokenInputVector(self):
         return {tk : np.full(256, self.distribution) for tk in self.tokenInput()}
-
+    
+    def mergeFile(self):
+        print(self.vocab)
+        # with open('file.txt', 'a') as dati:
+            # for key, token in self.vocab.items():
+                # dati.write(f"the key {key} -> merge {token}\n")
 
 
 class Transformers(Tokenizer):
@@ -96,7 +114,9 @@ class Transformers(Tokenizer):
         # print(self.tokenInputVector())
 
 t =Transformers()
-print(t.decode(t.encode('uuuuuuuuuuuuuuuuuuufffffffffffff'), True))
+# t.encode('fffffffffffffffffffuuuuuuuuuuuuuuuu')
+print(t.decode(t.encode('uuuuuuu')))
+t.mergeFile()
 # print(t.decode([76,600], True))
 # print(t.decode(t.encode('cici'), True))
         
